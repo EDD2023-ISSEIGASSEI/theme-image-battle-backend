fa7fad67fe08042e9c73e7444bb2289a01cb68f7
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-25 18:00:53 +0900

    update: デモ用にline Bot側の処理を仮実装

---
+++ backend/handler/line_demo.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"line-bot-otp-back/logic"
+	"line-bot-otp-back/model"
+	"line-bot-otp-back/util"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+type LineDemoHandler struct{}
+
+type LineDemoRequest struct {
+	LineUid string `json:"lineUid"`
+}
+
+func (*LineDemoHandler) GenerateLineRegistrationOtp(ctx *gin.Context) {
+	var req LineDemoRequest
+	err := ctx.Bind(&req)
+	if err != nil {
+		s := err.Error()
+		r := util.BadRequest(&s)
+		log.Errorln("[Error]request parse error: ", s)
+		ctx.JSON(r.StatusCode, r.Message)
+		return
+	}
+
+	ll := logic.LineSessionLogic{
+		Session: model.LineSession{
+			LineUid: req.LineUid,
+		},
+	}
+	err = ll.Create()
+	if err != nil {
+		log.Errorln("[Error]exec error: ", err.Error())
+		r := util.InternalServerError(nil)
+		ctx.JSON(r.StatusCode, r.Message)
+		return
+	}
+	r := util.Ok(nil)
+	ctx.JSON(r.StatusCode, gin.H{"otp": ll.Session.Otp})
+}
