adfcb8a066e014d5b19313dedd0474f89dd0cd72
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-08 07:42:42 +0900

    update: 本番環境のredisへの接続を実装

--- db/redis.go
+++ db/redis.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"os"
 
@@ -12,10 +13,21 @@ import (
 var Redis *redis.Client
 
 func InitRedis() {
-	Redis = redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
-		DB:   0,
-	})
+	var op *redis.Options
+	if os.Getenv("ENV") == "dev" {
+		op = &redis.Options{
+			Addr: fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+			DB:   0,
+		}
+	} else if os.Getenv("ENV") == "prod" {
+		op = &redis.Options{
+			Addr:      fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+			Password:  os.Getenv("REDIS_PASSWORD"),
+			TLSConfig: &tls.Config{MinVersion: tls.VersionTLS12},
+			DB:        0,
+		}
+	}
+	Redis = redis.NewClient(op)
 
 	ctx := context.Background()
 	err := Redis.Ping(ctx).Err()
